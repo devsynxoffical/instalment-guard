93a0705123be0b9527f1d9f242fa21c9651a4ec5
Syed Hassan Ali Shah <hassan@Syeds-MacBook-Air.local>
2026-09-24 13:35:17 +0500

    fix(gradle): pin buildToolsVersion to 35.0.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 android {
     namespace = "com.example.installment_guard"
     compileSdk = flutter.compileSdkVersion
+    buildToolsVersion = "35.0.0"
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
